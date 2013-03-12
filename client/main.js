b1465ea292bea98d729fe01d6976483df5b48b08
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-12 14:11:07 -0700

    wip - pulled out lobby and room templates

--- client/main.js
+++ client/main.js
@@ -37,16 +37,25 @@ Template.allRooms.events = {
   }
 };
 
-Template.room.roomName = function() {
+Template.roomOverview.roomName = function() {
   var room = Rooms.findOne({_id: Session.get('currentRoom')});
   return room && room.name;
 };
 
-Template.room.currentCount = function() {
+Template.roomOverview.currentCount = function() {
   // var room = Rooms.findOne({_id: Session.get('currentRoom')});
   return Room.currentSize(Session.get('currentRoom'));
   // return room && room.currentCount;
 };
+
+// Template.roomUsers.user = function() {
+//   var userIds = Rooms.findOne({_id: Session.get('currentRoom')}, {users: 1, _id: 0} ).users,
+//       users = [];
+//   for (var i = 0; i < userIds.length; i++) {
+//     users.push(Meteor.users.findOne({_id: userIds[i] }));
+//   }
+//   return users;
+// };
 
 Template.room.events = {
   'click .leave': function() {
@@ -54,5 +63,3 @@ Template.room.events = {
     Meteor.users.update({_id: Meteor.userId() }, {$set:{"profile.currentRoom": null}});
   }
 };
-
-
