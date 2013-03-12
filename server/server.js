c4a7e55d1fbbdd1454d5b930c4e60ac5e40ec234
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-12 11:53:40 -0700

    about to abstract html helpers; wip

--- server/server.js
+++ server/server.js
@@ -1,17 +1,9 @@
 Meteor.setInterval(function(){
 
-  var now = new Date().getTime();
-  now = now - 4000;
-  // console.log(now);
-  var expiredUsers = LoggedUsers.find( {stamp: {$lt: now } } ).fetch();
-  console.log("in setInterval");
+  var time = new Date().getTime() - 10000;
+  var expiredUsers = LoggedUsers.find( {stamp: {$lt: time } } ).fetch();
   for (var i = 0; i < expiredUsers.length; i++) {
-    console.log("about to log expiredUsers");
-    console.log('this guy is expired', i + ' - ' + expiredUsers[i].id);
-    // console.log(expiredUsers[i]);
-    console.log(now);
-    console.log(expiredUsers[i].stamp < now);
     Rooms.update({}, {$pull: {'users': expiredUsers[i].id} } );
     LoggedUsers.remove({'id': expiredUsers[i].id});
   }
-}, 4000);
+}, 10000);
